4f72e779a68ada52dd2f1eba7cc47d5a2247aa91
tarasVarvariuk <taras20109@gmail.com>
2016-03-21 02:11:54 +0200

    added addmin part

---
+++ EasyTourAdmin/Scripts/AddCityScript.js
@@ -0,0 +1,24 @@
+function thailandClick(){
+    document.getElementById("ThailandCitiesList").style.visibility = "visible";
+}
+function itallyClick(){
+    document.getElementById("ItallyCitiesList").style.visibility = "visible";
+}
+function bangkockClick(){
+    document.getElementById("BangkockHotelsList").style.visibility = "visible";
+}
+function patayaClick(){
+    document.getElementById("PatayaHotelsList").style.visibility = "visible";
+}
+function veniceClick(){
+    document.getElementById("VeniceHotelsList").style.visibility = "visible";
+}
+function florenceClick(){
+    document.getElementById("FlorenceHotelsList").style.visibility = "visible";
+}
+function romeClick(){
+    document.getElementById("RomeHotelsList").style.visibility = "visible";
+}
+function displayTable(){
+    document.getElementById("AvailabilityTable").style.visibility = "visible";
+}
